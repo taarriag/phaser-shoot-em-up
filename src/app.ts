7ecd3110445619ab9b695e1a661e73a6ba24a8d0
Tomas Arriagada <taarriag@gmail.com>
2016-08-10 09:34:50 -0400

    Created base phaser + typescript + gulp project

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+/// <reference path="phaser.d.ts"/>
+
+class SimpleGame {
+    game : Phaser.Game;
+
+    constructor() {
+        this.game = new Phaser.Game(720, 480, Phaser.AUTO, 
+            'content', {preload : this.preload, create: this.create});
+    }
+
+    preload() {
+        this.game.load.image('logo', 'resources/phaser.png');
+    }
+
+    create() {
+        var logo = this.game.add.sprite(this.game.world.centerX,
+            this.game.world.centerY, 'logo');
+        logo.anchor.setTo(0.5, 0.5);
+    }
+}
+
+window.onload = () => {
+    var game = new SimpleGame();
+}
